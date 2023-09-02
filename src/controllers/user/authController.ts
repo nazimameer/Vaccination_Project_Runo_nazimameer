f4dba9e8e10525a0e6deb1881ef159393db6217c
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-02 20:50:18 +0530

    fix small bug

--- src/controllers/user/authController.ts
+++ src/controllers/user/authController.ts
@@ -15,17 +15,18 @@ export const loginUser = async (req: Request, res: Response) => {
 
     // Generate a JWT token
 
-        // Set user details
-    const payload = { 
+    // Set user details
+    const payload = {
       userId: user?._id,
       phoneNumber: user?.phoneNumber,
     };
-        // Set the token expiration time
+    // Set the token expiration time
     const expire = {
-      expiresIn: "1h", 
+      expiresIn: "1h",
     };
-        // Set secret key for verify
-    const secretKey = process.env.JWT_SECRET!; 
+    // Set secret key for verify
+    const secretKey = process.env.JWT_SECRET!;
+    // Create token
     const token = jwt.sign(payload, secretKey, expire);
 
     res.status(200).json({ message: "Login successful", token });
