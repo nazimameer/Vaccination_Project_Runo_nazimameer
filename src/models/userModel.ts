ec39af22ebc6d2f5ac1b555a560a552889ee4448
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-02 17:34:20 +0530

    User Model

---
+++ src/models/userModel.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+interface ISlot {
+  date: Date;
+  timeSlot: string;
+  status: string;
+}
+
+interface IUser extends Document {
+  name: String;
+  phoneNumber: string;
+  age: number;
+  pincode: string;
+  aadharNo: number;
+  password: string;
+  vaccinationStatus: string;
+  registeredSlot: Array<ISlot>;
+}
+
+const userSchema = new Schema<IUser>({
+  name: String,
+  phoneNumber: String,
+  age: Number,
+  pincode: String,
+  aadharNo: Number,
+  password: String,
+  vaccinationStatus: String,
+  registeredSlot: [
+    {
+      date: Date,
+      timeSlot: String,
+      status: String,
+    },
+  ],
+});
+
+const Users: Model<IUser> = mongoose.model<IUser>("User", userSchema);
+export default Users;
