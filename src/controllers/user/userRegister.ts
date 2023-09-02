18a6243376e9c9651867dcdb29d104be5c36e79c
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-03 02:53:20 +0530

    fix bugs

--- src/controllers/user/userRegister.ts
+++ src/controllers/user/userRegister.ts
@@ -4,8 +4,9 @@ import Users from "../../models/userModel";
 
 export const registerUser = async (req: Request, res: Response) => {
   try {
+
     // Extract the required fields from the request body
-    const { name, phoneNumber, age, pincode, aadharNo, password } = req.body;
+    const { phoneNumber, age, pincode, aadharNo, password, name } = req.body;
 
     // Check if the user already exists based on phoneNumber
     const userExist = await Users.findOne({ phoneNumber });
@@ -14,7 +15,7 @@ export const registerUser = async (req: Request, res: Response) => {
     }
 
     // Hash the password
-    const hashedPassword = bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     // Create a new user document
     const newUser = new Users({
@@ -29,7 +30,7 @@ export const registerUser = async (req: Request, res: Response) => {
     // Save the user document to the database
     await newUser.save();
 
-    res.status(201).json("User registered successfully");
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Error registering user" });
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import * as express from "express";
 const router = express.Router();
+import { registerUser, loginUser } from '../controllers/user'
 
-router.post("/api/users/register");
-router.post("/api/users/login");
+
+router.post("/api/users/register", registerUser);
+router.post("/api/users/login", loginUser);
 router.get("/api/users/slot");
 router.post("/api/users/register-login");
 router.put("/api/users/update-slot");
