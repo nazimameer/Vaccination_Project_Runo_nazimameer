c898c7301061e4713eca99736826eaccfae680ba
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-03 20:28:31 +0530

    slots available api add

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,6 +4,7 @@ const router = express.Router();
 router.post("/api/users/register");
 router.post("/api/users/login");
 router.get("/api/users/slot");
+router.get("/api/users/slot/availabe");
 router.post("/api/users/register-login");
 router.put("/api/users/update-slot");
 
