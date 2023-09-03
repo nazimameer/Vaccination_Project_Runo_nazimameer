38112ef82f0f8f7d76af5e709151c1653849c974
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-04 00:48:54 +0530

    vaccineModel dose change

--- src/models/vaccineSlotModel.ts
+++ src/models/vaccineSlotModel.ts
@@ -2,12 +2,14 @@ import mongoose, { Document, Model, Schema } from "mongoose";
 
 interface IVaccineSlot extends Document {
   date: Date;
+  dose: string;
   timeSlot: string;
   status: string;
 }
 
 const vaccineSlotSchema = new Schema<IVaccineSlot>({
   date: Date,
+  dose: String,
   timeSlot: String,
   status: String,
 });
