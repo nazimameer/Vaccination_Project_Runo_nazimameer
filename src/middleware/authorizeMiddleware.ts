d839b1c16be458a16ae0a9a5227ea418831b5a54
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-04 00:29:24 +0530

    authorizeMiddleware

---
+++ src/middleware/authorizeMiddleware.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+export const requireAdmin = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.user && req.user.role === "admin") {
+    return res.status(403).json({ message: "Access denied" });
+  }
+
+  next();
+};
