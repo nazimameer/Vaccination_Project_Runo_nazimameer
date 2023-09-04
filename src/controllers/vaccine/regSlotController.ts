78162a6a11e2a912d9b9aeba7b0c8b559a4143e4
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-04 15:57:05 +0530

    regSlots update status

--- src/controllers/vaccine/regSlotController.ts
+++ src/controllers/vaccine/regSlotController.ts
@@ -3,7 +3,7 @@ import Users from "../../models/userModel";
 import VaccineSlotModel, {
   ITimeSlot,
   IVaccineSlot,
-  RegisteredUser
+  RegisteredUser,
 } from "../../models/vaccineSlotModel";
 
 export const regVaccineSlot = async (req: Request, res: Response) => {
@@ -22,10 +22,19 @@ export const regVaccineSlot = async (req: Request, res: Response) => {
       return res
         .status(400)
         .json({ message: `You already completed all vaccination` });
-    }else if(user?.vaccinationStatus === "" && doseType === 'second-dose' ){
-        return res.status(400).json({ message: "You need to taken the first-dose before second-dose" })
-    }else if(user?.vaccinationStatus === "first-dose" && doseType === 'first-dose'){
-        return res.status(400).json({ message: "You already taken first-dose vaccination"})
+    } else if (user?.vaccinationStatus === "" && doseType === "second-dose") {
+      return res
+        .status(400)
+        .json({
+          message: "You need to taken the first-dose before second-dose",
+        });
+    } else if (
+      user?.vaccinationStatus === "first-dose" &&
+      doseType === "first-dose"
+    ) {
+      return res
+        .status(400)
+        .json({ message: "You already taken first-dose vaccination" });
     }
     // Define a function to check available doses for a specific date and time
     async function checkAvailableSlot(
@@ -56,39 +65,46 @@ export const regVaccineSlot = async (req: Request, res: Response) => {
 
     const doseAvailable = await checkAvailableSlot(date, timeSlot);
 
-    if(!doseAvailable){
-        return res.status(404).json({ message: "Sorry no dose available"});
+    if (!doseAvailable) {
+      return res.status(404).json({ message: "Sorry no dose available" });
     }
 
     const slotDetails = {
       phoneNumber: phoneNumber || "",
       time: timeSlot, // Replace with the actual time slot
       dose: doseType, // Replace with the dose information
+      status: "booked",
     };
-    const slotDetails2  = {
+    const slotDetails2 = {
       date: new Date(date),
       dose: doseType,
       timeSlot: timeSlot,
     };
 
     // Create a new vaccine slot document
-     await VaccineSlotModel.findOneAndUpdate(
-        {
-            date: new Date(date), // Match the date
-            "slots.time": timeSlot, // Match the time inside the slots array
-          },
-          {
-            $push: { "slots.$.registered_users": slotDetails }, // Push the data into the matched time slot
-          },
+    await VaccineSlotModel.findOneAndUpdate(
+      {
+        date: new Date(date), // Match the date
+        "slots.time": timeSlot, // Match the time inside the slots array
+      },
+      {
+        $push: { "slots.$.registered_users": slotDetails }, // Push the data into the matched time slot
+      }
     );
-    await Users.findByIdAndUpdate(
-      phoneNumber,
+    await Users.findOneAndUpdate(
+      { phoneNumber },
       {
         $push: { registeredSlot: slotDetails2 },
-      },
-      { new: true }
+      }
     );
 
+    // Update the user's vaccination status
+    if (user && doseType === 'first-dose') {
+        user.vaccinationStatus = 'first-dose';
+      } else if (user && doseType === 'second-dose') {
+        user.vaccinationStatus = 'completed';
+      }
+      await user?.save();
     return res.json({ message: "Slot registered successfully" });
   } catch (error) {
     console.log(error);
