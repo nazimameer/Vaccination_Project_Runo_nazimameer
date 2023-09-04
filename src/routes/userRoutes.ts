3cf36df48eede483504149de73839529443c2349
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-04 16:57:19 +0530

    add availbelR registerR

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,12 @@
 import * as express from "express";
 const router = express.Router();
 import { registerUser, loginUser } from '../controllers/user'
-
+import { getAvailableSlot, regVaccineSlot } from '../controllers/vaccine'
 
 router.post("/api/users/register", registerUser);
 router.post("/api/users/login", loginUser);
-router.get("/api/users/slot");
-router.get("/api/users/slot/availabe");
-router.post("/api/users/slot/register");
+router.get("/api/users/slot/availabe",getAvailableSlot);
+router.post("/api/users/slot/register", regVaccineSlot);
 router.put("/api/users/slot/update-slot");
 
 module.exports = router;
