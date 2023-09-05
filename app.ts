5cc5cf82ab3890bf1d74caf1392fb4b3a648b08d
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-05 08:50:04 +0530

    fix all bugs by testing

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import http from "http";
 import { dbconnect } from "./src/config/config";
 import dotenv from "dotenv";
+import { userRoutes, adminRoutes } from "./src/routes";
 dotenv.config();
 
 const app = express();
@@ -10,12 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const server = http.createServer(app);
+app.use(userRoutes, adminRoutes);
 
-const userRoutes = require("./src/routes/userRoutes");
-const adminRoutes = require("./src/routes/adminRoutes");
 
-app.use('/user', userRoutes);
-app.use('/adminRoutes', adminRoutes);
 dbconnect();
 
 const port = process.env.PORT || 8000;
--- src/controllers/admin/registeredSlots.ts
+++ src/controllers/admin/registeredSlots.ts
@@ -3,16 +3,8 @@ import Users, { IUser } from "../../models/userModel";
 
 export const fetchSlots = async (req: Request, res: Response) => {
   try {
-    Users.find({}, "registeredSlot", (err: any, users: IUser[]) => {
-      if (err) {
-        // Handle the error
-        console.log(err);
-        return res.status(404).json({ message: "No registered slots" });
-      } else {
-        // `users` will contain an array of user documents with only the `registeredSlot` field included
-        return res.status(200).json({ users });
-      }
-    });
+   const slots = await Users.find({}, "registeredSlot")
+    return res.status(200).json({ slots });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Error logging in" });
